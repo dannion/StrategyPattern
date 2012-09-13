f7d89c9910a66da35e958018df5f6606fca1ba90
Dannion <dannionqiu@dannionqiu.local>
2012-09-13 09:11:57 +0800

    Initial Commit

---
+++ StrategyPattern/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  StrategyPattern
+//
+//  Created by Dannion on 12-9-13.
+//  Copyright (c) 2012年 dannionQ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
